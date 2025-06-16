0fdc6088b18016e188794a6a11d292e54a01dfe9
SKim041 <sehee1806@gmail.com>
2025-06-16 15:41:33 +0900

    최종

---
+++ Source/HW05/Private/MyActor.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+#include "Components/StaticMeshComponent.h"
+
+DEFINE_LOG_CATEGORY(LogMyActor);
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 루트 컴포넌트로 설정
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = MeshComponent;
+}
+
+void AMyActor::CreateEvent() {
+	int RandomValue = FMath::RandRange(0, 100);
+	if (RandomValue <= this->EventProbability) {
+		this->CountEvnt++;
+		UE_LOG(LogMyActor, Warning, TEXT("Event Triggered!"));
+	}
+	else {
+		UE_LOG(LogMyActor, Warning, TEXT("Event Not Triggered"));
+	}
+}
+
+float AMyActor::Distance(FVector First, FVector Second) {
+	float Dist_X = First.X - Second.X;
+	float Dist_Y = First.Y - Second.Y;
+	float Dist = FMath::Sqrt(Dist_X * Dist_X + Dist_Y * Dist_Y);
+
+	UE_LOG(LogMyActor, Display, TEXT("Traveled Distance: %.3f"), Dist);
+
+	return Dist;
+}
+void AMyActor::Move() {
+	int X = Step();
+	int Y = Step();
+
+	TotalDist += Distance(Start, FVector(Start.X + X, Start.Y + Y, 0));
+
+	Start.X += X;
+	Start.Y += Y;
+
+	MoveCount++;
+	
+	UE_LOG(LogMyActor, Display, TEXT("Current Location: %s"), *(Start.ToString()));
+
+	SetActorLocation(Start * 100);
+	CreateEvent();
+
+	// 10번 실행 후 타이머 해제
+	if (MoveCount == TotalMoveCount) {
+		UE_LOG(LogMyActor, Warning,
+			TEXT("Total Distance: %.3f, Total Event Count: %d"), TotalDist, CountEvnt);
+		GetWorld()->GetTimerManager().ClearTimer(MoveTimerHandle);
+	}
+}
+
+int32 AMyActor::Step() {
+	return FMath::RandRange(0,1);
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogMyActor, Warning, TEXT("Start Location: %s"), *(Start.ToString()));
+
+	//1초 마다 Move() 실행
+	GetWorld()->GetTimerManager().SetTimer(
+		MoveTimerHandle,
+		this,
+		&AMyActor::Move,
+		0.3f,
+		true
+	);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/HW05/Public/MyActor.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Math/UnrealMathUtility.h"
+#include "MyActor.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogMyActor, Log, All);
+
+UCLASS()
+class HW05_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+	void Move();
+	int32 Step();
+	float Distance(FVector First, FVector Second);
+	void CreateEvent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	int32 MoveCount = 0;
+
+	FTimerHandle MoveTimerHandle;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	int CountEvnt = 0;
+	float TotalDist = 0;
+
+	UPROPERTY(EditAnywhere)
+	FVector Start = FVector(0, 0, 0);
+
+	UPROPERTY(EditAnywhere)
+	float EventProbability = 50;
+
+	UPROPERTY(EditAnywhere)
+	int32 TotalMoveCount = 10;
+
+
+};
